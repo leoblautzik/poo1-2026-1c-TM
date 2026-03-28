a8cf8f8a8ee00e069abdebcca6366c816900ece6
leoblautzik <leoblautzik@gmail.com>
2026-03-28 19:18:52 -0300

    tp_01 fechas con validación

--- tp_01/fechas.py
+++ tp_01/fechas.py
@@ -10,6 +10,10 @@ def ordena_fechas(f1, f2):
     dia2 = f2 // 1000000
     mes2 = f2 % 1000000 // 10000
 
+    if not es_fecha_valida(dia1, mes1, anio1) or not es_fecha_valida(dia2, mes2, anio2):
+        print("Fecha invalida")
+        return None, None
+    # Versión conceptual
     # if anio1 != anio2:
     #     if anio1 < anio2:
     #         anterior = f1
@@ -37,6 +41,7 @@ def ordena_fechas(f1, f2):
     #         else:
     #             print("Son la misma fecha")
 
+    # Version m'as a lo python comparando tuplas
     if (anio1, mes1, dia1) < (anio2, mes2, dia2):
         anterior = f1
         posterior = f2
@@ -48,8 +53,32 @@ def ordena_fechas(f1, f2):
     return anterior, posterior
 
 
+def es_bisiesto(anio: int) -> bool:
+    return (anio % 4 == 0 and anio % 100 != 0) or (anio % 400 == 0)
+
+
+def esta_dentro_del_rango(x: int, a: int, b: int) -> bool:
+    return a <= x <= b
+
+
+def es_fecha_valida(d: int, m: int, a: int) -> bool:
+    mes_valido = esta_dentro_del_rango(m, 1, 12)
+    dia_valido = esta_dentro_del_rango(d, 1, 31)
+    anio_valido = esta_dentro_del_rango(a, 1900, 2100)
+
+    if not (mes_valido and dia_valido and anio_valido):
+        return False
+
+    if m == 2:
+        return d <= 28 + es_bisiesto(a)
+    elif m in (4, 6, 9, 11):
+        return d <= 30
+    else:
+        return True
+
+
 def main():
-    f1 = 27032027
+    f1 = 27122027
     f2 = 27032026
     print(ordena_fechas(f1, f2))
 
